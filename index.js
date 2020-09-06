f2598246ff28b76c2f7c682183aa4805886c229c
Mikołaj Strusiński <mikistrus@gmail.com>
2020-09-06 22:51:03 +0200

    Added Image enlargement on the main page

---
+++ index.js
@@ -0,0 +1,10 @@
+var lightbox = document.getElementById('enlargedPhoto')
+var smallimg = document.getElementById('smallimg')
+
+smallimg.addEventListener('click', function(){
+    lightbox.style.display = "block";
+})
+
+lightbox.addEventListener('click', function(){
+    lightbox.style.display = "none";
+})
