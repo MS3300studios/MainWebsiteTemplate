723b2358a335cce3e2c456d5f9fa4a8c310bc04e
Mikołaj Strusiński <mikistrus@gmail.com>
2020-09-07 18:01:56 +0200

    Added Gallery Buttons and Image Controll
    Now the user can browse through the images using buttons.

--- gallery.js
+++ gallery.js
@@ -1,18 +1,73 @@
 var imgList = document.querySelectorAll('#img')
 var lightbox = document.getElementById('lightbox')
 var lightboxImg = document.getElementById('lightbox_img')
+var ultimateSource = null;
+var newCell = null;
+var direction = null;
+//buttons
+var left = document.getElementById('leftImgBtn')
+var right = document.getElementById('rightImgBtn')
+var x = document.getElementById('xImgBtn')
+//array of images
+var imgSources = []
+for(var i = 0; i < imgList.length; i++){
+    imgSources.push(imgList[i].currentSrc)
+}
+var lastCell = imgSources.length-1;
+var firstCell = 0;
+
 
 for(var i=0; i<imgList.length; i++){
     imgList[i].addEventListener('click', function(event){
         lightbox.style.display = "flex";
         lightbox.style.justifyContent = "center";
         lightboxImg.src = event.target.src;
+        ultimateSource = event.target.src;
     })
 }
 
-lightbox.addEventListener('click', function(){
+x.addEventListener('click', function(){
     lightbox.style.display = "none";
 })
+
+function changeImage(currSrc, dir){
+    var cell = imgSources.indexOf(currSrc)
+
+    if(cell == lastCell){
+        if(dir == false){
+            newCell = cell-1;
+            lightboxImg.src = imgSources[newCell]
+            ultimateSource = imgSources[newCell]
+        }
+    }
+    else if(cell == firstCell){
+        if(dir == true){
+            newCell = cell+1;
+            lightboxImg.src = imgSources[newCell]
+            ultimateSource = imgSources[newCell]
+        }
+    }
+    else{
+        if(dir == true) newCell = cell+1;
+        else newCell = cell-1;
+    
+        lightboxImg.src = imgSources[newCell]
+        ultimateSource = imgSources[newCell]
+    }
+}
+
+
+addEventListener('click', function(event){
+     if(event.target == right){
+        direction = true;
+        changeImage(ultimateSource, direction);
+     }
+     else if(event.target == left){
+        direction = false;
+        changeImage(ultimateSource, direction);
+     }
+})
+
 
 
 
